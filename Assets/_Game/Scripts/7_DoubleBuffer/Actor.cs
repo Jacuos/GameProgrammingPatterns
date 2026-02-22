271a2d6046bb7995c3cee8b714d8fa322894428b
Jacuos <jacek.kozieja@gmail.com>
2026-02-22 14:16:49 +0100

    Pattern #7 - Double buffer

---
+++ Assets/_Game/Scripts/7_DoubleBuffer/Actor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace DoubleBuffer
+{
+    public class Actor : MonoBehaviour
+    {
+        private Actor _target;
+        private bool _currentSlapped;
+        private bool _nextSlapped;
+        private float _lastSlapTimestamp;
+
+        public void SetTarget(Actor target)
+        {
+            _target = target;
+        }
+
+        public void Slap()
+        {
+            _nextSlapped = true;
+            _lastSlapTimestamp = Time.time;
+        }
+
+        public void SwapBuffer()
+        {
+            _currentSlapped = _nextSlapped;
+            _nextSlapped = false;
+        }
+        
+        public void Execute()
+        {
+            if (_currentSlapped)
+            {
+                Debug.Log(gameObject.name+": Ouch! I got Slapped! Now I Slap: "+_target.gameObject.name);
+                _target.Slap();
+            }
+            else
+                Debug.Log(gameObject.name+": Nothing happened.");
+        }
+
+        void Update()
+        {
+            if(Time.time <1)
+                return;
+            transform.localScale = Vector3.Lerp(Vector3.one*1.5f, Vector3.one, (Time.time - _lastSlapTimestamp)*2f);
+        }
+        
+    }
+}
---
+++ Assets/_Game/Scripts/7_DoubleBuffer/Stage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace DoubleBuffer
+{
+    public class Stage : MonoBehaviour
+    {
+        [SerializeField]
+        private Actor[] _actors;
+        private Actor[] _scrambledActors;
+        private void Awake()
+        {
+            SetActorTargets();
+            ScrambleActorsOrder();
+            _scrambledActors[0].Slap();
+            StartCoroutine(DelayedUpdate());
+        }
+
+        void SetActorTargets()
+        {
+            for (int i = 0; i < _actors.Length; i++)
+            {
+                int targetiD = i + 1;
+                if (targetiD >= _actors.Length)
+                    targetiD = 0;
+                _actors[i].SetTarget(_actors[targetiD]);
+            }
+        }
+
+        void ScrambleActorsOrder()
+        {
+            _scrambledActors = new Actor[_actors.Length];
+            Random r = new Random();
+            int j = 0;
+            foreach (int i in Enumerable.Range(0, _actors.Length).OrderBy(x => r.Next()))
+            {
+                _scrambledActors[j] = _actors[i];
+                j++;
+            }
+        }
+
+        IEnumerator DelayedUpdate()
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            for(int i = 0; i < _scrambledActors.Length; i++)
+                _scrambledActors[i].Execute();
+            for(int i = 0; i < _scrambledActors.Length; i++)
+                _scrambledActors[i].SwapBuffer();
+            StartCoroutine(DelayedUpdate());
+        }
+        
+        
+        
+    }
+}
