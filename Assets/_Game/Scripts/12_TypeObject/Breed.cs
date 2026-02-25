69ef34d69d359d04f800897ef3a1933cd35eab96
Jacuos <jacek.kozieja@gmail.com>
2026-02-25 16:29:12 +0100

    Pattern #12 - Type Object

---
+++ Assets/_Game/Scripts/12_TypeObject/Breed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace TypeObject
+{
+	[CreateAssetMenu(menuName = "Config/Breed")]
+    public class Breed : ScriptableObject
+    {
+		public int health;
+        public string attackDescription;
+    }
+}
---
+++ Assets/_Game/Scripts/12_TypeObject/Monster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TypeObject
+{
+    public class Monster : MonoBehaviour
+    {
+        public Breed breed;
+        public int currentHealth;
+
+        void Awake()
+        {
+            currentHealth = breed.health;
+            Attack();
+        }
+
+        void Attack()
+        {
+            Debug.Log(breed.attackDescription);
+        }
+
+    }
+}
