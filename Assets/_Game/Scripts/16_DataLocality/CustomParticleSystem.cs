72a0295a3156507a3b3421966100d979564ae6f5
Jacuos <jacek.kozieja@gmail.com>
2026-03-03 11:38:22 +0100

    Pattern #16 - DataLocality

---
+++ Assets/_Game/Scripts/16_DataLocality/CustomParticleSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class CustomParticleSystem : MonoBehaviour
+    {
+        private ParticleComponent[] _particles;
+
+        private void Awake()
+        {
+            _particles = GetComponentsInChildren<ParticleComponent>();
+        }
+
+        void Update()
+        {
+            for(int i=0;i<_particles.Length;i++)
+                _particles[i].Render();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/16_DataLocality/ParticleComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class ParticleComponent : MonoBehaviour
+    {
+        public float speed=1;
+        public void Render()
+        {
+            transform.position += Random.onUnitSphere * (speed * Time.deltaTime);
+        }
+    }
+}
